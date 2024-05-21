140c692b92bab9ae92bae511c1c079f9bbcfc671
mukappalambda <maokailan24@gmail.com>
2024-05-21 13:53:22 +0800

    test(server): add main_test.go

---
+++ server/main_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/mukappalambda/my-trader/gen/message/v1"
+)
+
+func TestMessageService(t *testing.T) {
+	ln, err := net.Listen("tcp", ":")
+	if err != nil {
+		t.Fatalf("failed to listen: %q", err)
+	}
+	t.Cleanup(func() {
+		ln.Close()
+	})
+	s := grpc.NewServer()
+	t.Cleanup(func() {
+		s.Stop()
+	})
+	pb.RegisterMessageServiceServer(s, &server{})
+	go func() {
+		log.Println(ln.Addr().String())
+		if err := s.Serve(ln); err != nil {
+			log.Fatalf("failed to serve: %q\n", err)
+		}
+	}()
+
+	conn, err := grpc.NewClient(ln.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("failed to create a new client: %q\n", err)
+	}
+	t.Cleanup(func() {
+		conn.Close()
+	})
+	c := pb.NewMessageServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	t.Cleanup(func() {
+		cancel()
+	})
+	message := "test-message"
+	r, err := c.PutMessage(ctx, &pb.MessageRequest{
+		Topic:   "test-topic",
+		Message: message,
+	})
+	if err != nil {
+		t.Fatalf("failed to call PutMessage: %q\n", err)
+	}
+	got := r.GetMessage()
+	want := "hello" + message
+
+	if got != want {
+		t.Fatalf("got: %v; want: %v\n", got, want)
+	}
+}
