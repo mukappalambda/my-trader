2c3562cff1feec389bf29558550c9d1ae0e9ffe7
mukappalambda <maokailan24@gmail.com>
2024-06-02 10:17:52 +0800

    chore(script): add table schema on container creation

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+CONTAINER_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_DB=demo
+WORKDIR=/var/lib/postgresql
+
 docker run -dt \
-  --name postgres \
+  --name ${CONTAINER_NAME} \
   -p 5432:5432 \
-  -e POSTGRES_USER=postgres \
+  -e POSTGRES_USER=${POSTGRES_USER} \
   -e POSTGRES_PASSWORD=password \
-  -e POSTGRES_DB=demo \
+  -e POSTGRES_DB=${POSTGRES_DB} \
   postgres:14
+
+docker cp internal/db/messages/schema.sql postgres:${WORKDIR}
+sleep 1
+docker exec -u ${POSTGRES_USER} -w ${WORKDIR} ${CONTAINER_NAME} bash -c "psql -f schema.sql -d ${POSTGRES_DB}"
+docker exec -u ${POSTGRES_USER} -w ${WORKDIR} ${CONTAINER_NAME} bash -c "psql -d ${POSTGRES_DB} -c '\dt'"
