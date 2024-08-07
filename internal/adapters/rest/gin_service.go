68d4d9cbbccd94a8d808a581c61ae9d9d7ed3a3e
mukappalambda <maokailan24@gmail.com>
2024-08-07 16:23:28 +0800

    feature(adapters): add the rest adapter for the schema registry server

---
+++ internal/adapters/rest/gin_service.go
@@ -0,0 +1,85 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/mukappalambda/my-trader/internal/adapters/rest/types"
+)
+
+type SchemaRegistryServer struct {
+	Engine *gin.Engine
+}
+
+func NewSchemaRegistryServer(ginMode string) (*SchemaRegistryServer, error) {
+	if ginMode != "debug" && ginMode != "release" {
+		return nil, fmt.Errorf("got invalid gin mode: %q (want 'debug' or 'release')", ginMode)
+	}
+	if ginMode == "debug" {
+		gin.SetMode(gin.DebugMode)
+	}
+	if ginMode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	engine := gin.Default()
+	err := engine.SetTrustedProxies(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set trusted proxies: %v", err)
+	}
+	engine.Use(cors.Default())
+	setupRoutes(engine)
+	engine.Use(HandlerNotFound())
+	return &SchemaRegistryServer{
+		Engine: engine,
+	}, nil
+}
+
+func setupRoutes(engine *gin.Engine) {
+	schemaRouterGroup := engine.Group("/schemas")
+	{
+		schemaRouterGroup.GET("", GetAllSchemas())
+		schemaRouterGroup.POST("", NewSchema())
+		schemaRouterGroup.GET("/:id", GetSchemaById())
+	}
+}
+
+func HandlerNotFound() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "not found",
+		})
+	}
+}
+
+func GetAllSchemas() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var schemas []types.Schema
+		c.JSON(http.StatusOK, schemas)
+	}
+}
+
+func NewSchema() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var schema types.Schema
+		if err := c.BindJSON(&schema); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"reason": "invalid schema",
+			})
+			return
+		}
+		c.JSON(http.StatusCreated, schema)
+	}
+}
+
+func GetSchemaById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	}
+}
+
+func (srv *SchemaRegistryServer) Run(addr ...string) error {
+	return srv.Engine.Run(addr...)
+}
