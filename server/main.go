81cac1532be01c2e70ce6c8a23280898c6f7043d
mukappalambda <maokailan24@gmail.com>
2024-08-07 16:24:52 +0800

    feature(server): spin up the schema registry server in main()

--- server/main.go
+++ server/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	grpcAdapter "github.com/mukappalambda/my-trader/internal/adapters/grpc"
+	restAdapter "github.com/mukappalambda/my-trader/internal/adapters/rest"
 )
 
-var port = flag.Int("port", 50051, "server port")
+var (
+	port    = flag.Int("port", 50051, "server port")
+	ginMode = flag.String("gin-mode", "release", "gin mode (debug|release)")
+)
 
 func main() {
 	flag.Parse()
@@ -17,6 +22,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	schemaRegistryServer, err := restAdapter.NewSchemaRegistryServer(*ginMode)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	registryPort := 8081
+	go func() {
+		log.Printf("schema registry server is listening at port: %d\n", registryPort)
+		if err := schemaRegistryServer.Run(fmt.Sprintf("localhost:%d", registryPort)); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	if err := srv.Run(*port); err != nil {
 		log.Fatal(err)
 	}
