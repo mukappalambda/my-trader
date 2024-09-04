dcfbfb79ea8caf5ffdbc17f9645df2d5f680b82f
mukappalambda <maokailan24@gmail.com>
2024-09-04 14:46:50 +0800

    ✨ Feature(commands): add the `--schema` flag to the `send` command (#14)

--- client/commands/commands.go
+++ client/commands/commands.go
@@ -97,7 +97,43 @@ func RunSend(cmd *cobra.Command, args []string) {
 	topic, _ := cmd.Flags().GetString("topic")
 	message, _ := cmd.Flags().GetString("message")
 	serverUrl, _ := cmd.Flags().GetString("server-url")
+	srUrl, _ := cmd.Flags().GetString("schema-registry-url")
+	schemaName, _ := cmd.Flags().GetString("schema")
 
+	params := url.Values{}
+	params.Set("name", schemaName)
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/schemas?%s", srUrl, params.Encode()), nil)
+	common.PrintToStderrThenExit(err)
+	client := &http.Client{
+		Timeout: 200 * time.Millisecond,
+	}
+	resp, err := client.Do(req)
+	common.PrintToStderrThenExit(err)
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	common.PrintToStderrThenExit(err)
+	var schema types.Schema
+	err = json.Unmarshal(body, &schema)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not deserialize to the given schema: %v\n", err)
+		os.Exit(1)
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(message), &result)
+	common.PrintToStderrThenExit(err)
+	collected := make(map[string]interface{})
+	for _, field := range schema.Fields {
+		if field.Type == "double" {
+			if value, ok := result[field.Name].(float64); ok {
+				collected[field.Name] = value
+			}
+		}
+		if field.Type == "string" {
+			if value, ok := result[field.Name].(string); ok {
+				collected[field.Name] = value
+			}
+		}
+	}
 	conn, err := grpc.NewClient(serverUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -107,7 +143,7 @@ func RunSend(cmd *cobra.Command, args []string) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.PutMessage(ctx, &pb.MessageRequest{
+	_, err = c.PutMessage(ctx, &pb.MessageRequest{
 		Topic:     topic,
 		Message:   message,
 		CreatedAt: toDateTime(time.Now()),
@@ -115,8 +151,16 @@ func RunSend(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
-
+	fmt.Printf("Sent message: schema-subject=%s schema-name=%s ", schema.Subject, schema.Name)
+	for k, v := range collected {
+		switch typedV := v.(type) {
+		case float64:
+			fmt.Printf("%s=%f ", k, typedV)
+		case string:
+			fmt.Printf("%s=%s ", k, typedV)
+		}
+	}
+	fmt.Printf("\n")
 }
 
 func RunGet(cmd *cobra.Command, args []string) {
