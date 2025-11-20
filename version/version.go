68cc4e53ec782a44db34a457dd9aac934b97229f
Mao-Kai Lan <maokailan24@gmail.com>
2025-11-20 16:09:13 +0800

    ✨ feature(cmd): implement and consolidate version for cmd (#41)
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- version/version.go
+++ version/version.go
@@ -1,7 +1,38 @@
 package version
 
-var (
-	Commit  = "unknown"
-	Date    = "unknown"
-	Version = "unknown"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"text/tabwriter"
 )
+
+var (
+	GitCommit  = "unknown"
+	GitVersion = "unknown"
+	BuildDate  = "unknown"
+	RepoUrl    = "unknown"
+)
+
+func Version(name string) {
+	fmt.Printf("%s%s\n\n", name, RepoUrl)
+
+	type KV struct {
+		K string
+		V string
+	}
+	kvs := []KV{
+		{K: "GitVersion", V: GitVersion},
+		{K: "GitCommit", V: GitCommit},
+		{K: "BuildDate", V: BuildDate},
+		{K: "GoVersion", V: runtime.Version()},
+		{K: "Compiler", V: runtime.Compiler},
+		{K: "Platform", V: fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)},
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 1, 8, 1, ' ', 0)
+	defer tw.Flush()
+	for _, kv := range kvs {
+		fmt.Fprintf(tw, "%s:\t%s\n", kv.K, kv.V)
+	}
+}
